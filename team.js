30fe0a86931d00c5c0b15a171647cb05a70ba3fb
Trentham3269 <scottdalwood@gmail.com>
2017-02-17 16:07:51 +1100

    Initial commit of html and data files

---
+++ team.js
@@ -0,0 +1,22 @@
+var dataset = [
+  [ "Tiger Nixon", "Human Resources Director", "Newcastle", "x12345" ],
+  [ "Garrett Winters", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Ashton Cox", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Cedric Kelly", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Airi Satou", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Brielle Williamson", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Herrod Chandler", "Data Analysis Manager", "Newcastle", "x12345" ],
+  [ "Rhona Davidson", "Data Analysis Manager", "Penrith", "x12345" ],
+  [ "Colleen Hurst", "Data Analysis Manager", "Sydney", "x12345" ],
+  [ "Sonya Frost", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Jena Gaines", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Quinn Flynn", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Charde Marshall", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Haley Kennedy", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Tatyana Fitzpatrick", "Data Analyst", "Newcastle", "x12345" ],
+  [ "Michael Silva", "Data Analyst", "Penrith", "x12345" ],
+  [ "Paul Byrd", "Data Analyst", "Penrith", "x12345" ],
+  [ "Gloria Little", "Data Analyst", "Penrith", "x12345" ],
+  [ "Bradley Greer", "Data Analyst", "Penrith", "x12345" ],
+  [ "Dai Rios", "Data Analyst", "Penrith", "x12345" ]
+];
